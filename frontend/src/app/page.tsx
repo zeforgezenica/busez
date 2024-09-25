f4a3a075868f5f591085e9ea5b6b8b3a4b88cc77
Kemal Sivro <kemalsivro@gmail.com>
2024-09-25 17:59:04 +0200

    app name change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -289,7 +289,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Head>
-        <title>Busez</title>
+        <title>kadJeBus</title>
       </Head>
       <div className="container mx-auto p-4">
         <h1 className="text-3xl font-bold text-center mb-4">Busez</h1>
