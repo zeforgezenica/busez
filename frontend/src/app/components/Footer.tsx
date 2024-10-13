fde1aecab56d8b1397039a21acfad4b8ebee29bc
Nine <abdelrhmanshalil@gmail.com>
2024-10-13 18:47:22 +0200

    Refactor

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 import {
   Modal,
   ModalContent,
@@ -9,24 +9,24 @@ import {
   Button,
   Input,
   Textarea,
-} from "@nextui-org/react";
-import emailService from "../services/email.service";
+} from '@nextui-org/react';
+import emailService from '../services/email.service';
 
 const Footer: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [formData, setFormData] = useState({
-    name: "",
-    contactInfo: "",
-    subject: "",
-    details: "",
+    name: '',
+    contactInfo: '',
+    subject: '',
+    details: '',
   });
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
   const handleOpen = () => {
     setIsOpen(true);
-    setError("");
-    setSuccess("");
+    setError('');
+    setSuccess('');
   };
   const handleClose = () => setIsOpen(false);
 
@@ -43,7 +43,7 @@ const Footer: React.FC = () => {
     const { name, subject, details } = formData;
 
     if (!name || !subject || !details) {
-      setError("Molimo popunite sva obavezna polja (*).");
+      setError('Molimo popunite sva obavezna polja (*).');
       return;
     }
 
@@ -52,138 +52,138 @@ const Footer: React.FC = () => {
         subject: formData.subject,
         text: `
           Ime: ${formData.name}
-          Kontakt: ${formData.contactInfo || "N/A"}
+          Kontakt: ${formData.contactInfo || 'N/A'}
           Detalji: ${formData.details}
         `,
         senderName: formData.name,
         senderContact: formData.contactInfo,
       });
 
-      setSuccess("Vaša poruka je uspješno poslana.");
-      setError("");
-      setFormData({ name: "", contactInfo: "", subject: "", details: "" });
+      setSuccess('Vaša poruka je uspješno poslana.');
+      setError('');
+      setFormData({ name: '', contactInfo: '', subject: '', details: '' });
     } catch (error) {
-      setError("Došlo je do greške prilikom slanja poruke.");
+      setError('Došlo je do greške prilikom slanja poruke.');
     }
   };
 
   return (
     <>
-      <footer className="bg-content1 border-t border-gray-300 mt-auto py-4">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col items-center justify-between sm:flex-row">
-            <div className="text-center sm:text-left">
-              <p className="text-gray-300">
-                &copy; {new Date().getFullYear()}{" "}
+      <footer className=' border-t border-neutral-800 mt-auto py-4'>
+        <div className='container mx-auto px-4'>
+          <div className='flex flex-col items-center justify-between sm:flex-row'>
+            <div className='text-center sm:text-left'>
+              <p className='text-gray-300'>
+                &copy; {new Date().getFullYear()}{' '}
                 <a
-                  href={"https://zeforge.ba"}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href={'https://zeforge.ba'}
+                  target='_blank'
+                  rel='noopener noreferrer'
                   style={{
-                    color: "var(--primary-blue)",
-                    textDecoration: "underline",
+                    color: 'var(--primary-blue)',
+                    textDecoration: 'underline',
                   }}
                 >
                   ZeForge Zenica
                 </a>
               </p>
-              <p className="text-gray-300">
+              <p className='text-gray-300'>
                 Aplikacija za pretraživanje autobusnih linija
               </p>
             </div>
-            <div className="mt-4 sm:mt-0 text-left">
-              <p className="text-left sm:text-center">
+            <div className='mt-4 sm:mt-0 text-left'>
+              <p className='text-left sm:text-center'>
                 <strong>Kontaktirajte nas:</strong>
               </p>
-              <p className="text-gray-300 text-left">
-                E-pošta:{" "}
+              <p className='text-gray-300 text-left'>
+                E-pošta:{' '}
                 <a
-                  href={`mailto:${"info@zeforge.ba"}`}
+                  href={`mailto:${'info@zeforge.ba'}`}
                   style={{
-                    color: "var(--primary-blue)",
-                    textDecoration: "underline",
+                    color: 'var(--primary-blue)',
+                    textDecoration: 'underline',
                   }}
                 >
                   info@zeforge.ba
                 </a>
               </p>
-              <p className="text-gray-300 text-left">
-                Broj telefona:{" "}
+              <p className='text-gray-300 text-left'>
+                Broj telefona:{' '}
                 <a
-                  href={`tel:${"+38732979844"}`}
+                  href={`tel:${'+38732979844'}`}
                   style={{
-                    color: "var(--primary-blue)",
-                    textDecoration: "underline",
+                    color: 'var(--primary-blue)',
+                    textDecoration: 'underline',
                   }}
                 >
                   +387 32 979 844
                 </a>
               </p>
 
-              <Button onPress={handleOpen} color="primary" className="mt-4">
+              <Button onPress={handleOpen} color='primary' className='mt-4'>
                 Prijavite problem / Predložite funkciju
               </Button>
             </div>
           </div>
-          <div className="my-4 border-t border-gray-300" />
-          <div className="text-center">
-            <p className="text-gray-300">
+          <div className='my-4 border-t border-gray-300' />
+          <div className='text-center'>
+            <p className='text-gray-300'>
               Napravljeno sa ❤️ od strane open source zajednice Zenice
             </p>
           </div>
         </div>
       </footer>
 
-      <Modal isOpen={isOpen} onClose={handleClose} placement="top-center">
+      <Modal isOpen={isOpen} onClose={handleClose} placement='top-center'>
         <ModalContent>
           <>
-            <ModalHeader className="flex flex-col gap-1">
+            <ModalHeader className='flex flex-col gap-1'>
               Prijavite problem / Predložite funkciju
             </ModalHeader>
             <ModalBody>
-              {error && <p className="text-red-500">{error}</p>}
-              {success && <p className="text-green-500">{success}</p>}
+              {error && <p className='text-red-500'>{error}</p>}
+              {success && <p className='text-green-500'>{success}</p>}
               <Input
                 autoFocus
-                label="Ime *"
-                name="name"
+                label='Ime *'
+                name='name'
                 value={formData.name}
                 onChange={handleChange}
-                placeholder="Unesite Vaše ime"
-                variant="bordered"
+                placeholder='Unesite Vaše ime'
+                variant='bordered'
                 required
               />
               <Input
-                label="Kontakt Informacije (Nije obavezno)"
-                name="contactInfo"
+                label='Kontakt Informacije (Nije obavezno)'
+                name='contactInfo'
                 value={formData.contactInfo}
                 onChange={handleChange}
-                placeholder="E-pošta ili Telefon (Nije obavezno)"
-                variant="bordered"
+                placeholder='E-pošta ili Telefon (Nije obavezno)'
+                variant='bordered'
               />
               <Input
-                label="Predmet *"
-                name="subject"
+                label='Predmet *'
+                name='subject'
                 value={formData.subject}
                 onChange={handleChange}
-                placeholder="Unesite naslov poruke"
-                variant="bordered"
+                placeholder='Unesite naslov poruke'
+                variant='bordered'
                 required
               />
               <Textarea
-                label="Detalji *"
-                name="details"
+                label='Detalji *'
+                name='details'
                 value={formData.details}
                 onChange={handleChange}
-                placeholder="Opišite problem ili prijedlog"
+                placeholder='Opišite problem ili prijedlog'
                 required
               />
             </ModalBody>
             <ModalFooter>
-              <Button color="danger" variant="flat" onPress={handleClose}>
+              <Button color='danger' variant='flat' onPress={handleClose}>
                 Zatvori
               </Button>
-              <Button color="primary" onPress={handleSubmit}>
+              <Button color='primary' onPress={handleSubmit}>
                 Pošalji
               </Button>
             </ModalFooter>
