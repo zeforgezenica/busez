ce75cb91d4b47dc5167f6d971daa682fdd31eafb
Mora Obradovic <123646568+morascode@users.noreply.github.com>
2024-10-25 19:26:04 +0200

    Sort stations in combo box alphabetically (#139)
    * feat(global): add toSortedStationsAlphabetically fn;
    
    * chore(global): remove unnecessary parentheses;
    
    * fixed the root station to always appear on top
    
    ---------
    
    Co-authored-by: Amar Cerim <amar-cerim@hotmail.com>

--- frontend/src/app/lib/utils.ts
+++ frontend/src/app/lib/utils.ts
@@ -1,6 +1,16 @@
+import Station from '@/models/station.model';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function toSortedStationsAlphabetically(stations:Station[]):Station[] {
+  const rootStationName = 'Zenica "AS"'
+
+  return stations.sort((a, b) => {
+    if (a.name === rootStationName || b.name === rootStationName) return 1;
+    return a.name.localeCompare(b.name);
+  });
 }
