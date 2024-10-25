c61544cadae98a63bc17306fb4749ee73f640e59
Amar Cerim <amar-cerim@hotmail.com>
2024-10-25 19:10:31 +0200

    fixed the root station to always appear on top

--- frontend/src/app/lib/utils.ts
+++ frontend/src/app/lib/utils.ts
@@ -7,5 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function toSortedStationsAlphabetically(stations:Station[]):Station[] {
-  return stations.toSorted((a,b)=>a.name.localeCompare(b.name));
+  const rootStationName = 'Zenica "AS"'
+
+  return stations.sort((a, b) => {
+    if (a.name === rootStationName || b.name === rootStationName) return 1;
+    return a.name.localeCompare(b.name);
+  });
 }
