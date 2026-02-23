9116555ad06a4e912ad4126defe166d60a602af8
mbalicc <meho.balic.23@size.ba>
2026-02-23 14:27:17 +0100

    Add offline support

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv';
+import withSerwistInit from '@serwist/next';
 
 dotenv.config();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    turbopack: {}
+};
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+    // Ovdje se definira gdje stoji tvoj Service worker kod
+    swSrc: "src/app/sw.ts",
+    // Gdje će ga Serwist iskompajlirati
+    swDest: "public/sw.js",
+    // Gasi service worker u dev modu da ti ne smeta dok kucaš kod
+    disable: process.env.NODE_ENV === "development",
+});
+
+export default withSerwist(nextConfig);
